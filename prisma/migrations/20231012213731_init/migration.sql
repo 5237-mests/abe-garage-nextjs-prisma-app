ba19a973c155b60f90ed34aa34fb64e1ed82ad18
5237-mests <msfnmulgeta@gmail.com>
2023-10-13 01:01:23 +0300

    SQLite changed to PostgreSQL.

--- prisma/migrations/20231012213731_init/migration.sql
+++ prisma/migrations/20231012213731_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "Employee_test" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "firtName" TEXT,
     "lastName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Employee_test_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
